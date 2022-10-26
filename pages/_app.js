15229fd2372eb570b84af8159fb33a387e33bbf5
hunfa <83254423+hunfa@users.noreply.github.com>
2022-10-26 23:19:22 +0500

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import Navbar from '../components/Navbar'
+import '../styles/globals.css'
+import theme from "../theme.js"
+import { ChakraProvider } from '@chakra-ui/react'
+
+function MyApp({ Component, pageProps }) {
+
+  
+  return (
+  <>
+  <ChakraProvider theme={theme}>
+<Navbar/>
+  <Component {...pageProps} />
+  </ChakraProvider>
+  </>
+  )
+}
+
+export default MyApp
