7280ea116b5be879989b81db1e5c929d0cc03e1f
hunfa <nabeelmuzaffar0323@hotmail.com>
2022-10-28 20:04:48 +0500

    addition in navbar0

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,12 +10,48 @@ import {
   Image,
   InputGroup,
   InputLeftElement,
-  Input,
+  Input, Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,Icon
 } from "@chakra-ui/react";
 import { BiSearchAlt } from "react-icons/bi";
-import { Search2Icon } from "@chakra-ui/icons";
+import { BsQuestionSquare,BsQuestionLg } from "react-icons/bs"
+import { FiUser } from "react-icons/fi";
+import { FiTag } from "react-icons/fi";
+import { HiCode } from "react-icons/hi";
+import Script from "next/script";
 function Navbar() {
+  let fl=false;
+  const show=()=>{
+   
+    
+    if(fl===false){
+      
+      document.getElementById('one').style.transform='rotate(45deg) translate(8px)';
+      document.getElementById('two').style.opacity='0';
+      document.getElementById('two').style.transform='translate(-20px)';
+      document.getElementById('three').style.transform='rotate(-45deg) translate(9px)';
+      fl=true;       
+      }
+      else{
+          
+          document.getElementById('one').style.transform='rotate(0deg) translate(0px)';
+      document.getElementById('two').style.opacity='1';
+      document.getElementById('two').style.transform='translate(0px)';
+      document.getElementById('three').style.transform='rotate(0deg) translate(0px)';
+            fl=false;
+          }
+  }
+  
+
   return (
+    <>
+  
     <Flex
       p="2"
       align={"center"}
@@ -23,19 +59,45 @@ function Navbar() {
       px="4"
       justify={"space-between"}
       style={{ boxShadow: " -1px 0px 20px 0px rgba(99,93,255,0.2)" }}
+      bg={"white"}
+      position="relative"
     >
       {/* 1st component */}
       <Flex align={"center"} minW="215px" justify={"space-between"}>
-        <Flex
+
+
+        <Menu  autoSelect={false} closeOnBlur={false} closeOnSelect={false}>
+  <MenuButton onClick={show} >
+  <Flex 
           h={"20px"}
           w="24px"
           direction={"column"}
           justifyContent="space-between"
+        
         >
-          <Box h="5px" bg="gray.400" borderRadius={"8px"}></Box>
-          <Box h="4px" bg="gray.400" borderRadius={"8px"}></Box>
-          <Box h="5px" bg="gray.400" borderRadius={"8px"}></Box>
+          <Box id="one" h="5px" bg="gray.400" borderRadius={"8px"} style={{transition: "transform 0.3s"}}></Box>
+          <Box id="two" h="4px" bg="gray.400" borderRadius={"8px"} style={{transition: "opacity 0.3s, transform 0.3s"}}></Box>
+          <Box id="three" h="5px" bg="gray.400" borderRadius={"8px"} style={{transition: "transform 0.3s"}}></Box>
         </Flex>
+  </MenuButton>
+  <MenuList  position={"relative"} zIndex={"-1"} bottom="-13px" left="-15px"
+   border="none"
+   bg="white"
+   boxShadow={"none"}
+    py="5"
+    >
+    <MenuItem my="5px" fontWeight={"semibold"} icon={<BsQuestionLg size={"18px"} color="#635dff" /> } borderRight="2px solid #635dff"  >Questions</MenuItem>
+    <MenuItem my="5px" fontWeight={"semibold"} icon={<FiTag size={"18px"} color="#635dff" /> }>Tags</MenuItem>
+    <MenuItem my="5px" fontWeight={"semibold"} icon={<FiUser size={"18px"} color="#635dff" /> }>Users</MenuItem>
+    <MenuItem my="5px" fontWeight={"semibold"} icon={<HiCode size={"18px"} color="#635dff" /> }>Online Compiler</MenuItem>
+    <MenuItem my="5px" fontWeight={"semibold"} icon={<BsQuestionSquare size={"18px"} color="#635dff" />  }>About</MenuItem>
+  </MenuList>
+</Menu>
+
+
+
+
+
 
         <Box h="30px">
           <Image h="100%" src="/images/logo-high.png" alt="logoImage"></Image>
@@ -52,7 +114,7 @@ function Navbar() {
         <InputGroup>
           <InputLeftElement
             pointerEvents="none"
-            children={<BiSearchAlt color="rgb(99, 93, 255)" size={"24px"} />}
+            children={<Icon as={BiSearchAlt} color="rgb(99, 93, 255)" boxSize={"24px"}/> }
           />
           <Input
             border="1px"
@@ -89,6 +151,7 @@ function Navbar() {
       </Box>
       {/* 3rd component */}
     </Flex>
+    </>
   );
 }
 
