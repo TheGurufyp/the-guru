d68029441e5fb172d0096d835d09425be530bd8d
TheGurufyp <116750570+TheGurufyp@users.noreply.github.com>
2022-11-02 23:47:25 +0500

    Top Question
    check i have added top question page and component

---
+++ components/Topquestion.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Divider,
+  Heading,
+  Tag,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import { AiFillLike } from "react-icons/ai";
+import { RiMessage3Fill } from "react-icons/ri";
+import { ViewIcon } from "@chakra-ui/icons";
+export const Topquestion = () => {
+  return (
+    <>
+      <Flex
+        border={"1px"}
+        borderColor="gray.200"
+        justify="space-between"
+        align={"center"}
+        p="1rem"
+        borderRadius={"8px"}
+      >
+        <Flex
+          // border={"1px"}
+          direction="column"
+        >
+          <Heading size={"md"} color="#635DFF">
+            WHats's your Favorite dev tool?
+          </Heading>
+          <Box my={"0.7rem"} mr="1rem">
+            <Tag
+              // border={"1px"}
+              borderColor="#635DFF"
+              borderRadius={"8px"}
+            >
+              Technology
+            </Tag>
+            <Tag
+              // border={"1px"}
+              mx="0.7rem"
+              borderRadius={"8px"}
+              borderColor="#635DFF"
+            >
+              tools
+            </Tag>
+          </Box>
+          <Box display={"inherit"}>
+            <Image
+              borderRadius="full"
+              boxSize="30px"
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />{" "}
+            <Box fontWeight={"sm"} mx="4px">
+              hunfa modify 1 min ago
+            </Box>
+          </Box>
+        </Flex>
+        <Flex
+          // border={"1px"}
+          justify="space-between"
+        >
+          <Box
+            className="sideIcon"
+            // border={"1px"}
+            display="flex"
+            flexDirection={"column"}
+            alignItems="center"
+            mx={"0.5rem"}
+          >
+            <Text>100</Text>
+            <Text>Votes</Text>
+            <AiFillLike fill="#635DFF" />
+            {/* <Image>xyz</Image> */}
+          </Box>
+          {/* <Divider orientation="vertical" ></Divider> */}
+
+          <Box
+            // border={"1px"}
+            display="flex"
+            flexDirection={"column"}
+            alignItems="center"
+            mx={"0.9rem"}
+          >
+            <Text>200</Text>
+            <Text>Views</Text>
+            {/* <Image>xyz</Image> */}
+            <ViewIcon />
+          </Box>
+          <Box
+            // border={"1px"}
+            display="flex"
+            flexDirection={"column"}
+            alignItems="center"
+            mx={"0.5rem"}
+          >
+            <Text>10</Text>
+            <Text>Answers</Text>
+            <RiMessage3Fill fill="#635DFF" />
+            {/* <Image>xyz</Image> */}
+          </Box>
+        </Flex>
+      </Flex>
+    </>
+  );
+};
---
+++ pages/topquestion.js
@@ -0,0 +1,64 @@
+import { React } from "react";
+import {
+  Flex,
+  Box,
+  Divider,
+  Heading,
+  Tag,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { AiFillLike } from "react-icons/ai";
+import { RiMessage3Fill } from "react-icons/ri";
+import { ViewIcon } from "@chakra-ui/icons";
+import { Topquestion } from "../components/Topquestion";
+
+const Question = () => {
+  return (
+    <>
+      <Flex direction={"column"}>
+        <Heading
+          // border={"1px"}
+          size="lg"
+          width={"80%"}
+          marginInline="auto"
+          my={"2rem"}
+        >
+          Top Questions
+        </Heading>
+
+        <Tabs width="75%" marginInline={"auto"}>
+          <TabList
+            // border={"1px"}
+            display="flex"
+            justifyContent={"space-evenly"}
+          >
+            <Tab className="questionTabs">Top Question</Tab>
+            <Tab className="questionTabs">Unanswerd</Tab>
+            <Tab className="questionTabs">Newest</Tab>
+            <Tab className="questionTabs">Votes</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <Topquestion />
+            </TabPanel>
+
+            <TabPanel>
+              <Topquestion />
+            </TabPanel>
+            <TabPanel>
+              <Topquestion />
+            </TabPanel>
+            <TabPanel>
+              <Topquestion />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Flex>
+    </>
+  );
+};
+
+export default Question;
